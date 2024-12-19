d496da512f45506b64abf9ef7ef60d11a5e4d0b2
Manishis022 <78021811+Manishis022@users.noreply.github.com>
2024-12-19 19:34:37 +0530

    Delete MacAddress.ps1

--- MacAddress.ps1
+++
@@ -1,4 +0,0 @@
-﻿$interfaceName = "Wi-Fi"  # Name of the network interface
-$macAddress = (Get-NetAdapter -Name $interfaceName).MacAddress
-
-Write-Host "MAC Address of $interfaceName is $macAddress"
