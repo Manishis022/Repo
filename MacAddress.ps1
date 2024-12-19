8f6087e49a0af216deffaf0eb82eade44588a6f0
Manish Kumar <manishis022@gmail.com>
2024-12-19 10:48:48 +0530

    Mac Address

---
+++ MacAddress.ps1
@@ -0,0 +1,4 @@
+﻿$interfaceName = "Wi-Fi"  # Name of the network interface
+$macAddress = (Get-NetAdapter -Name $interfaceName).MacAddress
+
+Write-Host "MAC Address of $interfaceName is $macAddress"
